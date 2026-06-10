6df89c09857539015f0f32711fd252e5b6d03380
buj <buj351@outlook.com>
2026-06-10 22:05:02 +0700

    fuckton of logs cuz by now I'm losing my mind

--- src/main/kotlin/mindurka/coreplugin/Teams.kt
+++ src/main/kotlin/mindurka/coreplugin/Teams.kt
@@ -16,6 +16,8 @@ import mindurka.api.on
 import mindurka.api.timer
 import mindurka.coreplugin.commands.SpectateEnabled
 import mindurka.util.BoolRef
+import mindurka.util.UnsafeNull
+import mindurka.util.notnull
 import mindustry.Vars
 import mindustry.core.NetServer
 import mindustry.game.EventType
@@ -25,38 +27,37 @@ import mindustry.gen.Player
 import mindustry.net.Administration
 import kotlin.uuid.ExperimentalUuidApi
 
-private val assignDerelict = BoolRef(false)
+private var assignDerelict = false
+private val vanillaTeamAssigner = Vars.netServer.assigner
+private var restoreTeams = ObjectIntMap<String>()
+private val restoreSpectator: ObjectSet<String> = ObjectSet()
 
-fun teamAssigned(player: Player) = !assignDerelict.r &&
+fun teamAssigned(player: Player) = !assignDerelict &&
     (!Gamemode.enableSpectate || !Gamemode.spectate[player])
 
 @OptIn(ExperimentalUuidApi::class)
 fun initTeams() {
-    val vanillaTeamAssigner = Vars.netServer.assigner
-    var restoreTeams = ObjectIntMap<String>()
-    var restoreSpectator: ObjectSet<Player>? = null
 
     on<EventType.PlayEvent> { restoreTeams = ObjectIntMap<String>() }
-    on<RoundEndEvent> { // IDEA cannot predict that 'restoreSpectator' is ever modified otherwise lmao
-        val s = ObjectSet<Player>()
+    on<RoundEndEvent> {
         for (player in Groups.player) {
-            if (Gamemode.enableSpectate && Gamemode.spectate[player]) s.add(player)
+            if (Gamemode.enableSpectate && Gamemode.spectate[player]) restoreSpectator.add(player.sessionData.profileId)
         }
-        restoreSpectator = s
     }
     on<EventType.PlayerLeave> {
-        restoreSpectator?.remove(it.player)
+        val profileId = it.player.sessionData.profileId
+        restoreSpectator.remove(profileId)
         if (!teamAssigned(it.player)) return@on
-        restoreTeams.put(it.player.sessionData.profileId, it.player.team().id)
+        restoreTeams.put(profileId, it.player.team().id)
     }
 
     on<EventType.PlayEvent> {
         if (!Gamemode.randomizeTeams) return@on
         if (!Vars.state.rules.pvp) return@on
 
-        assignDerelict.r = true
+        assignDerelict = true
         timer(3f, lifetime = Lifetime.Round) {
-            assignDerelict.r = false
+            assignDerelict = false
             val vec = Groups.player.copy()
             vec.shuffle()
             for (player in vec) {
@@ -79,16 +80,27 @@ fun initTeams() {
     Vars.netServer.admins.addActionFilter { action -> action.type != Administration.ActionType.respawn || teamAssigned(action.player) }
 
     Vars.netServer.assigner = NetServer.TeamAssigner { player, players ->
-        if (restoreSpectator?.remove(player) == true) return@TeamAssigner Gamemode.spectate.spectateRestore(player, null)
-        if (assignDerelict.r) return@TeamAssigner Team.derelict
+        Log.info("[DEBUG/Team] Assigning some team to ${Strings.stripColors(player.name)}")
+
+        if (restoreSpectator.remove(player.sessionData.profileId)) {
+            Log.info("[DEBUG/Team] Assigned spectator")
+            return@TeamAssigner Gamemode.spectate.spectateRestore(player, null)
+        }
+        if (assignDerelict) {
+            Log.info("[DEBUG/Team] Assigning derelict team")
+            return@TeamAssigner Team.derelict
+        }
 
         if (!Vars.state.isPaused && Gamemode.restoreTeams) restoreTeams.get(player.sessionData.profileId, -1).let { id ->
+            Log.info("[DEBUG/Team] Trying to restore team")
             if (id == -1) return@let
             val team = Team.all[id]
             if (!team.data().isAlive) return@let
+            if (team == Team.derelict) return@let
             return@TeamAssigner team
         }
 
+        Log.info("[DEBUG/Team] No shortcut hit")
         val event = PlayerTeamAssign(
             player,
             players,
@@ -102,7 +114,11 @@ fun initTeams() {
 @Command
 @EnabledIf(SpectateEnabled::class)
 private fun spectate(caller: Player) {
-    Gamemode.spectate[caller] = !Gamemode.spectate[caller]
+    val setSpectate = !Gamemode.spectate[caller]
+    Gamemode.spectate[caller] = setSpectate
+
+    if (setSpectate) restoreSpectator.add(caller.sessionData.profileId)
+    else restoreSpectator.remove(caller.sessionData.profileId)
 }
 
 // TODO: Automatic spectator
